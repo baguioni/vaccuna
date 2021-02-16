7065fcd6714a54536647b23a6ce084a798e89afd
Vincent Alec F. Baguio <vfbaguio@up.edu.ph>
2021-02-16 21:06:07 +0800

    add: initial homepage view

--- registrant/views.py
+++ registrant/views.py
@@ -1,3 +1,4 @@
 from django.shortcuts import render
 
-# Create your views here.
+def RegistrantHome(request, *args, **kwargs):
+    return render(request, "home.html", {})
--- vaccuna/urls.py
+++ vaccuna/urls.py
@@ -16,6 +16,9 @@ Including another URLconf
 from django.contrib import admin
 from django.urls import path
 
+from registrant.views import RegistrantHome
+
 urlpatterns = [
+    path('', RegistrantHome),
     path('admin/', admin.site.urls),
 ]
