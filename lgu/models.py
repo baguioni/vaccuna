08d9087f173abaca7c05666e8e756f2f34233c28
Vincent Alec F. Baguio <vfbaguio@up.edu.ph>
2021-02-20 15:48:19 +0800

    add: get coordinates model

--- lgu/models.py
+++ lgu/models.py
@@ -1,3 +1,13 @@
 from django.db import models
+from core.models import AddressField
 
-# Create your models here.
+
+class LocalGovernmentUnit(models.Model):
+    name = models.CharField(max_length=50)
+    registrant_map = models.FileField(upload_to='maps/', null=True)
+
+
+class VaccinationSite(models.Model):
+    name = models.CharField(max_length=50)
+    address = models.OneToOneField(AddressField, on_delete=models.CASCADE)
+    lgu = models.ForeignKey(LocalGovernmentUnit, on_delete=models.CASCADE)
