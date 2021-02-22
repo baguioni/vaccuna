7fa90b6fa3fce5055dcac984a7a55c25bb257a92
Zedec <zedec.pacaldo@gmail.com>
2021-02-22 15:53:58 +0800

    Update: Registration Form with Sign Up style

--- registrant/forms.py
+++ registrant/forms.py
@@ -9,6 +9,10 @@ class AddressFieldForm(ModelForm):
     class Meta:
         model = AddressField
         fields = '__all__'
+        widgets = {
+            'username': forms.TextInput(attrs={'class': 'input'}),
+            'email': forms.EmailInput(attrs={'class': 'input'}),
+        }
 
     line1 = forms.CharField(initial='Street Address 1')
     line2 = forms.CharField(initial='Street Address 2')
@@ -16,14 +20,17 @@ class AddressFieldForm(ModelForm):
                   widget=forms.Select(attrs={'id': 'region'}),
                   required=False,
                 )
+
     province = forms.CharField(label=('Province'),
                   widget=forms.Select(attrs={'id': 'province'}),
                   required=False,
                 )
+
     city = forms.CharField(label=('City/Municipality'),
                   widget=forms.Select(attrs={'id': 'city'}),
                   required=False,
                 )
+
     barangay = forms.CharField(label=('Barangay'),
                   widget=forms.Select(attrs={'id': 'barangay'}),
                   required=False,
