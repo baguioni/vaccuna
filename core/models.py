ca7ea2f753a0b1a36a9d52b906a0535afa2fcdeb
Mark Louis Tutor <mark.louis.tutor@eee.upd.edu.ph>
2021-03-02 00:55:03 +0800

    added sms function in models.py

--- core/models.py
+++ core/models.py
@@ -1,7 +1,11 @@
+import qrcode
+import cv2 as cv
+from PIL import Image
 from django.contrib.auth.models import AbstractUser
 from django.db import models
 import googlemaps
 from enum import IntEnum
+import requests
 
 
 class PriorityGroup(IntEnum):
@@ -22,10 +26,68 @@ class PriorityGroup(IntEnum):
     B6 = 11
     C = 12
 
-
     @classmethod
     def choices(cls):
         return [(key.value, key.name) for key in cls]
+
+
+# GENEREATE AND READ QR
+
+
+class QR(models.Model):
+    name = models.CharField(max_length=250)
+    qr_code = models.ImageField(upload_to=None)
+
+    def __str__(self):
+        return str(self.name)
+
+    def generateQR(self, *args, **kwargs):
+        qr = qrcode.QRCode(
+            version=1,
+            error_correction=qrcode.constants.ERROR_CORRECT_H,
+            box_size=10,
+            border=4,
+        )
+        qr.add_data(self.name)  # data here
+        qr.make(fit=True)
+        img = qr.make_image(fill_color="black",
+                            back_color="white").convert('RGB')
+        # image overlay code start (remove if not needed)
+        logo_display = Image.open('Vaccuna Logo.png')
+        logo_display.thumbnail((150, 150))
+        logo_pos = ((img.size[0] - logo_display.size[0]) // 2,
+                    (img.size[1] - logo_display.size[1]) // 2)
+        img.paste(logo_display, logo_pos)
+        # image overlay code end
+        # img.save("qr00001.png")
+        return img
+
+    def readQR(img):
+        im = cv.imread('sample2.png')
+        det = cv.QRCodeDetector()
+        retval, points, straight_qrcode = det.detectAndDecode(im)
+        # result is stored in retval ,return retval
+        return retval
+# GENEREATE AND READ QR
+
+# my_num = "+639470676215"  -> sample number, number is declared as string not int
+#my_message = "Good day Juan Dela Cruz! This is to inform you of your scheduled vaccination on March 2, 2021 at PGH. Addt'l reminders here"
+#sms(my_num, my_message)
+
+
+def sms(number, message):
+    url = "https://rest-api.d7networks.com/secure/send"
+    payload = "{\n\t\"to\":\"%s\",\n\t\"content\": \" %s \", \n\t\"from\": \"Vaccuna\", \n\t\"dlr\": \"yes\", \n\t\"dlr-method\": \"GET\", \n\t\"dlr-level\": \"2\", \n\t\"dlr-url\": \"http://yourcustompostbackurl.com\"\n}" % (
+        number, message)
+    headers = {
+        'Content-Type': 'application/x-www-form-urlencoded',
+        'Authorization': 'Basic dWZsbjM2MTU6VXVmdk9XSno='
+    }
+
+    response = requests.request(
+        "POST", url, headers=headers, data=payload)  # get api respons
+    # print(response.text.encode('utf8'))  # prints api response(success or not)
+    return
 
 
 class User(AbstractUser):
@@ -34,15 +96,20 @@ class User(AbstractUser):
 
 
 class AddressField(models.Model):
+    line1 = models.CharField('Street Address 1', max_length=50)
+    line2 = models.CharField(
+        'Street Address 2', max_length=50, blank=True, default='')
     region = models.CharField(max_length=50)
     province = models.CharField(max_length=50)
     city = models.CharField('City / Municipality', max_length=50)
     barangay = models.CharField(max_length=50)
     line1 = models.CharField('Street Address 1', max_length=50)
-    line2 = models.CharField('Street Address 2', max_length=50, blank=True, default='', null=True)
-    latitude = models.DecimalField(max_digits=18, decimal_places=15, null=True, blank=True)
-    longitude = models.DecimalField(max_digits=18, decimal_places=15, null=True, blank=True)
-
+    line2 = models.CharField(
+        'Street Address 2', max_length=50, blank=True, default='', null=True)
+    latitude = models.DecimalField(
+        max_digits=18, decimal_places=15, null=True, blank=True)
+    longitude = models.DecimalField(
+        max_digits=18, decimal_places=15, null=True, blank=True)
 
     def get_formatted_address(self):
         parts = [', '.join(filter(bool, [self.line1, self.line2]))]
@@ -51,7 +118,8 @@ class AddressField(models.Model):
 
     def get_full_address(self):
         parts = [', '.join(filter(bool, [self.line1, self.line2]))]
-        parts.append(', '.join((self.barangay, self.city, self.province, self.region)))
+        parts.append(
+            ', '.join((self.barangay, self.city, self.province, self.region)))
         return ' '.join((x.strip() for x in parts if x))
 
     def get_inline_address(self):
