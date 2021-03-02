767fbf06d98d71774326a17e41da8ff1cf9be9c0
Vincent Alec F. Baguio <vfbaguio@up.edu.ph>
2021-03-02 21:19:55 +0800

    refactor: add vaccination site map

--- lgu/tasks.py
+++ lgu/tasks.py
@@ -6,16 +6,28 @@ from core.models import AddressField
 from lgu.models import LocalGovernmentUnit
 from vaccuna import settings
 
-def generate_registrant_markers_map(lgu):
-    # Get address of registrants of LGU
-    addresses = lgu.registrants.all().values('address',)
+
+def get_coordinates(addresses):
     coordinates = []
     for address in addresses:
         obj = AddressField.objects.get(id=address['address'])
         coordinates.append((float(obj.latitude), float(obj.longitude)))
     coordinates_lats, coordinates_long = zip(*coordinates)
+
+    return coordinates_lats, coordinates_long
+
+
+def generate_registrant_markers_map(lgu):
+    # Get address of registrants of LGU
+    addresses = lgu.registrants.all().values('address',)
+    coordinates_lats, coordinates_long = get_coordinates(addresses)
     gmap = gmplot.GoogleMapPlotter(float(lgu.latitude), float(lgu.longitude), 14, apikey=GOOGLE_MAPS_API_KEY)
     gmap.scatter(coordinates_lats, coordinates_long, color='cornflowerblue')
+
+    vaccination_sites = lgu.vaccination_sites.all().values('address',)
+    coordinates_lats, coordinates_long = get_coordinates(vaccination_sites)
+    gmap.scatter(coordinates_lats, coordinates_long, color='red')
+
     html = ContentFile(gmap.get())
     lgu.registrant_map.save(f'{lgu.name}Map.html', html)
 
